da0fe86f724664713c86d41ef07f160b1c858521
angelicagrass <angelicagrass@hotmail.com>
2021-10-14 15:41:02 +0200

    created questionclass

---
+++ src/questionSentence.js
@@ -0,0 +1,14 @@
+export default class QuestionSentence {
+  constructor(words, index) {
+    this.words = words
+    this.sentence = ''
+    this.index = `index ${index}: `
+  }
+
+  toString() {
+    this.words.forEach(element => {
+      this.sentence += element + ' '
+    })
+  }
+  
+}
