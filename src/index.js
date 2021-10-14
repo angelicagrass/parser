18844117d56a3245db0fd50d4ef30693886d2395
angelicagrass <angelicagrass@hotmail.com>
2021-10-14 15:40:38 +0200

    changes in index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import { Tokenizer } from 'angelicagrass-tokenizer/src/tokenizer.js'
 import { regex } from './grammar/sentenceGrammar.js'
+import RegularSentence from './regularSentence.js'
+import ExclamationSentence from './exclamationSentence.js'
+import QuestionSentence from './questionSentence.js'
+import Parser from './parser.js'
+import PrettyPrinter from './prettyPrinter.js'
 
 
-const TheTokenizer = new Tokenizer(regex, 'ett ! litet test bara.')
-
-
-
+const TheTokenizer = new Tokenizer(regex, 'ett ! litet test bara. har jag rätt? eller har jag kommit fel? sista meningen!')
 TheTokenizer.setTokenizerRules()
-let the = TheTokenizer.tokenizeTheString() // array med object resultat
-TheTokenizer.getPreviousToken()
+let sentences = TheTokenizer.tokenizeTheString() // array med object resultat
 
-console.log(the)
+const parser = new Parser(sentences).parse()
+const printer = new PrettyPrinter(parser)
+printer.colorize()
 
-// komma åt inuti objekten
-the.map(obj => {
-  if (obj.type === 'WORD')  console.log(obj.type)
-})
+
+
+
+
+
+
+
+
+
