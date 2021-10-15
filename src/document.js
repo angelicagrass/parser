cea2fd7ca21c72cd450d719bcab3eb08e89edcb0
angelicagrass <angelicagrass@hotmail.com>
2021-10-15 10:49:09 +0200

    filter methods to get different sentences.

--- src/document.js
+++ src/document.js
@@ -1,19 +1,37 @@
-import QuestionSentence from "./questionSentence.js"
+
+import RegularSentence from './regularSentence.js'
+import ExclamationSentence from './exclamationSentence.js'
+import QuestionSentence from './questionSentence.js'
 
 export default class Document {
   constructor(allSentences) {
     this.allSentences = allSentences
-
+    this.choosenSentences = []
   }
 
   getAllSentences() {
     return this.allSentences
   }
 
-  getExclamationSentences() {}
+  getExclamationSentences() {
+    this.allSentences = this.allSentences.filter(element => element instanceof ExclamationSentence)
 
-  getRegularSentences() {}
+    // console.log(this.allSentences + 'här är exclam')
+    // this.allSentences.forEach(element => {
+    //   console.log(element + 'här är element')
+    //   // this.allSentences.filter(element instanceof ExclamationSentence)
+    //   // return this.allSentences 
+    // })
+    // // return this.choosenSentences
+    // // this.allSentences.filter(element instanceof ExclamationSentence)
+    // return this.allSentences
+  }
 
-  getQuestionSentences() {}
+  getRegularSentences() {
+    this.allSentences = this.allSentences.filter(element => element instanceof RegularSentence)
+  }
 
+  getQuestionSentences() {
+    this.allSentences = this.allSentences.filter(element => element instanceof QuestionSentence) 
+  }
 }
