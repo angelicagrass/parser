0c9fede23007be9cffc056bd0a0e2bd966f20544
angelicagrass <angelicagrass@hotmail.com>
2021-10-15 11:21:52 +0200

    created sentences.js

---
+++ src/sentences/sentence.js
@@ -0,0 +1,15 @@
+export default class Sentence  {
+  constructor(words, index) {
+    this.words = words
+    this.sentence = ''
+    this.index = `index ${index}: `
+    
+  }
+
+  toString() {
+    this.words.forEach(element => {
+      this.sentence += element + ' '
+    })
+  }
+  
+}
